8a667a85987f65bcd96ed35772b8eb880c2df7cc
etudiants <abcd@mail.com>
2023-10-20 14:09:28 +0200

    👀

--- src/Lists/ArrayList.php
+++ src/Lists/ArrayList.php
@@ -18,23 +18,81 @@ class ArrayList implements ListInterface
         return json_encode($this->elements, JSON_PRETTY_PRINT);
     }
 
-    public function push(mixed $element = null): void {}
+    public function push(mixed $element = null): void
+    {
+        if (!empty($this->elements) && gettype($element) !== gettype($this->elements[0])) {
+            throw new \InvalidArgumentException("Invalid element type");
+        }
+        $this->elements[] = $element;
+    }
 
-    public function get(int $index): mixed {}
+    public function get(int $index): mixed
+    {
+        if ($this->isValidIndex($index)) {
+            return $this->elements[$index];
+        }
 
-    public function set(int $index, mixed $element): void {}
+        throw new \OutOfBoundsException("Invalid index: $index");
+    }
 
-    public function clear(): void {}
+    public function set(int $index, mixed $element): void
+    {
+        if ($this->isValidIndex($index)) {
+            if (gettype($element) !== gettype($this->elements[0])) {
+                throw new \InvalidArgumentException("Invalid element type");
+            }
+            $this->elements[$index] = $element;
+        } else {
+            throw new \OutOfBoundsException("Invalid index: $index");
+        }
+    }
 
-    public function includes(mixed $element): bool {}
+    public function clear(): void
+    {
+        $this->elements = [];
+    }
 
-    public function isEmpty(): bool {}
+    public function includes(mixed $element): bool
+    {
+        return in_array($element, $this->elements);
+    }
 
-    public function indexOf(mixed $element): int {}
+    public function isEmpty(): bool
+    {
+        return empty($this->elements);
+    }
 
-    public function remove(int $index): void {}
+    public function indexOf(mixed $element): int
+{
+    $index = array_search($element, $this->elements, true);
+    if ($index === false) {
+        throw new \OutOfBoundsException("Element not found");
+    }
+    return $index;
+}
 
-    public function size(): int {}
 
-    public function toArray(): array {}
+    public function remove(int $index): void
+    {
+        if ($this->isValidIndex($index)) {
+            array_splice($this->elements, $index, 1);
+        } else {
+            throw new \OutOfBoundsException("Invalid index: $index");
+        }
+    }
+
+    public function size(): int
+    {
+        return count($this->elements);
+    }
+
+    public function toArray(): array
+    {
+        return $this->elements;
+    }
+
+    protected function isValidIndex(int $index): bool
+    {
+        return $index >= 0 && $index < count($this->elements);
+    }
 }
